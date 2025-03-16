cf88de6e02638660b703b954b9a0e675309cea27
devjenirfs <jeniffer@thecaselygroup.com>
2025-03-16 12:26:53 -0300

    imagens e videos

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+    return (
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>404 - Página não encontrada</h1>
+        <p>Ops! A página que você está procurando não existe.</p>
+      </div>
+    );
+  }
+  
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import { ThemeProvider } from "../components/ThemeProvider";
 import ThemeToggle from "../components/ThemeToggle";
 import ArmarioReserva from "../components/ArmarioReserva";
 import Noticias from "../components/Noticias";
+import Carrossel from "../components/Carrossel";  // 🆕 Carrossel de notícias
 import "./styles.css";
 
 export default function App() {
@@ -13,6 +15,7 @@ export default function App() {
                 <ThemeToggle />
                 <ArmarioReserva />
                 <Noticias />
+                <Carrossel/>
             </div>
         </ThemeProvider>
     );
